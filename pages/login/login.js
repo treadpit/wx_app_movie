2a7451569aeb6a7a5b8c17f5ac5d533bc148b313
Dengrong Fu <drfu@drfudeMac-mini.local>
2016-11-16 18:54:32 +0800

    提取设置页

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,38 +2,33 @@ const app = getApp();
 
 const conf = {
   data: {
-    logo: "http://p0.jmstatic.com/templates/jumei/images/logo_new_v1.jpg",
-    account: "",
-    password: "",
-    phone: "",
-    code: ""
+    logo: "http://p0.jmstatic.com/templates/jumei/images/logo_new_v1.jpg"
   },
-  _login() {
-
-    // ajax 请求
-    // fetch("", function())
-    // wx.showToast({
-    //   title: "正在登陆..."
-    // });
-      wx.navigateTo({
-				url: '../hot/hot',
-         success: function (res) {
-           console.log('跳转成功');
-           console.log(res);
-           wx.setStorage({
-             key: 'user',
-             data: {
-               'uid': '45213665',
-               'hasLogin': true
-             },
-             success: function (_res) {
-               // success
-               console.log('储存成功');
-               console.log(_res);
-             }
-           })
+  login() {
+    wx.showToast({
+      title: "请稍后..."
+    });
+    wx.navigateBack({
+      delta: 1,
+      success: function (res) {
+        // success
+        wx.setStorage({
+          key: 'user',
+          data: {
+            'uid': '45213665',
+            'hasLogin': true
+          },
+          success: function (_res) {
+          }
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
       }
-			})
+    })
   }
 };
 
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -2,7 +2,61 @@ var app = getApp();
 
 const conf = {
 	data: {
-		userInfo: '' // 可以不定义
+		userInfo: '', // 可以不定义
+		tabs: [
+			{
+				icon: "../../images/hot.png",
+				tit: "电影票",
+				types: "movie"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "演出票",
+				types: "show"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "影城卡",
+				types: "ciname"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "优惠券",
+				types: "privilege"
+			}
+		],
+		others: [
+			{
+				icon: "../../images/hot.png",
+				tit: "想看的电影",
+				types: "want"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "看过的电影",
+				types: "watched"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "帮助中心",
+				types: "help"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "意见反馈",
+				types: "feed"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "人工客服",
+				types: "service"
+			},
+			{
+				icon: "../../images/hot.png",
+				tit: "设置",
+				types: "setting"
+			}
+		]
 	},
 	onLoad() {
 
@@ -11,7 +65,7 @@ const conf = {
 		const self = this;
 		const userInfo = wx.getStorageSync('user');
 		if (!userInfo.hasLogin) {
-			wx.redirectTo({
+			wx.navigateTo({
 				url: '../login/login'
 			})
 		} else {
@@ -38,78 +92,28 @@ const conf = {
 			userInfo: ""
 		})
 	},
-	lightAode(e) {
-		const mode = e.detail.value ? '开启' : '关闭';
-		wx.showToast({
-			title: `已${mode}夜间模式`,
-			icon: 'success',
-			duration: 500
-		})
-	},
-	chooseAvatar(e) {
-		const self = this;
-		wx.showActionSheet({
-			itemList: ['拍照上传', '从相册选择'],
-			itemColor: '#e50150',
-			success(res) {
-				if (!res.cancel) {
-					if (res.tapIndex === 0) {
-						self.takePhoto();
-					} else {
-						self.chooseImageFromCamera();
-					}
-				}
-			},
-			fail() {
-				console.log(e);
-			}
-		})
-	},
-	chooseImageFromCamera() {
-		const self = this;
-		wx.chooseImage({
-			count: 1, // 最多可以选择的图片张数，默认9
-			sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
-			sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有， 若只写其中一个，则无论哪一个都是调用相册，不能调用相机
-			success: function (res) {
-				self.setData({
-					'userInfo.avatarUrl': res.tempFilePaths[0]
-				});
-				wx.showToast({
-					title: "头像更换成功",
-					duration: 1500
+
+	othersClicked(e) {
+		const _type = e.currentTarget.dataset.type;
+		switch (_type) {
+			case "service":
+				this.callMe();
+				break;
+			case "setting":
+				wx.navigateTo({
+				  url: './setting/setting',
+				  success: function(res){
+					// success
+				  },
+				  fail: function() {
+					// fail
+				  },
+				  complete: function() {
+					// complete
+				  }
 				})
-			},
-			fail: function () {
-				wx.showToast({
-					title: "头像设置失败",
-					duration: 1500
-				})
-			}
-		})
-	},
-	takePhoto() {
-		const self = this;
-		wx.chooseImage({
-			count: 1,
-			sizeType: ['original', 'compressed'],
-			sourceType: ['album', 'camera'],
-			success: function (res) {
-				self.setData({
-					'userInfo.avatarUrl': res.tempFilePaths[0]
-				});
-				wx.showToast({
-					title: "头像更换成功",
-					duration: 1500
-				})
-			},
-			fail: function () {
-				wx.showToast({
-					title: "头像设置失败",
-					duration: 1500
-				})
-			}
-		})
+				break;
+		}
 	},
 	callMe() {
 		wx.makePhoneCall({
